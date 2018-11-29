22d09272e1cf88d158c016556bec2c205e526445
Tristan <tristanbeedell@gmail.com>
2018-11-29 17:10:49 +0000

    finished day 1 2015

---
+++ 2015/1/part_2.py
@@ -0,0 +1,16 @@
+with open('input.txt', 'r') as input:
+	pos = 0
+	char_pos = 0
+	text = input.readline()
+	for char in text :
+		if char == ')':
+			pos -= 1
+		elif char == '(':
+			pos += 1
+		char_pos += 1
+		if pos == -1:
+			print (char_pos)
+			break
+	
+	input.close()
+
